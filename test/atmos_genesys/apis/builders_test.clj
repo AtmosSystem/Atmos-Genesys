80c19eac7c7cff3d852c18c19008632ae7d1ed07
Santiago de Pedro <santiago.pedro23@gmail.com>
2024-11-10 17:07:36 -0400

    Create new test suite for API response handling
    Added a new test suite 'builders_test.clj' to verify API response handling in various scenarios including valid responses, exceptions, and invalid specifications. The tests ensure appropriate status codes and exception maps are returned. Additionally, implemented a 'test-verbose?' flag for controllable logging during tests.

---
+++ test/atmos_genesys/apis/builders_test.clj
@@ -0,0 +1,60 @@
+(ns atmos-genesys.apis.builders-test
+  (:require
+    [atmos-genesys.apis.builders :as builder]
+    [clojure.spec.alpha :as s]
+    [clojure.test :refer :all])
+  (:import (java.util Map)))
+
+(def test-verbose? (Boolean/parseBoolean (System/getenv "TEST_VERBOSE")))
+
+(extend-protocol atmos-logs.core/PLoggerActions
+  Map
+  (error [_ data] (if test-verbose? (println data)))
+  (exception [_ data] (if test-verbose? (println data))))
+
+(deftest API-responses
+
+  (let [exception-message-keys [:type :extra-data :message]
+        code #(first (keys %))
+        data #(first (vals %))]
+
+
+    (testing "Method returns 200 status code when is valid"
+
+      (let [response (builder/try-ok-or-400 (+ 1 1))]
+
+        (is (= (code response) 200))))
+
+
+    (testing "Method returns 400 status code when exception occurs"
+
+      (let [response (builder/try-ok-or-400 (/ 1 0))]
+
+        (is (= (code response) 400))))
+
+
+    (testing "Method returns 400 status code and a valid exception map when exception occurs"
+
+      (let [response (builder/try-ok-or-400 (/ 1 0))]
+
+        (are [expected result] (= expected result)
+
+                               400 (code response)
+                               exception-message-keys (keys (data response)))))
+
+
+    (testing "Method returns 400 status code and a valid exception map when invalid spec occurs"
+
+      (let [response (builder/try-ok-or-400 (assert (s/valid? number? "w")))]
+
+        (are [expected result] (= expected result)
+
+                               400 (code response)
+                               exception-message-keys (keys (data response)))))
+
+
+    (testing "Method returns 404 status code when there is no result"
+
+      (let [response (builder/try-ok-or-404 nil)]
+
+        (is (= 404 (code response)))))))
